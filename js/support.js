7a1b6754fb34b92bb78e9a859a5106f9860c4305
Yousef <yousef.fatouraa@aut.ac.ir>
2018-05-16 01:13:26 +0430

    ajax_added

--- js/support.js
+++ js/support.js
@@ -1,20 +1,98 @@
+
 /**
  * Created by Yousef on 18/05/15.
  */
+picture="";
 function openChat() {
-    document.getElementById("support-closed").style.display="none";
-    document.getElementById("support-open").style.display="block";
+
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            var obj = JSON.parse(this.responseText);
+            if(obj.success == true){
+                document.getElementById("support-closed").style.display="none";
+                document.getElementById("support-open").style.display="block";
+                getSupport();
+            }else {
+                alert("can't connect");
+            }
+        }
+    };
+    xhttp.open("GET", "http://51.15.59.130:46260/start", true);
+    xhttp.send();
+    // getResponse();
 }
 function closeChat() {
     document.getElementById("support-closed").style.display="block";
     document.getElementById("support-open").style.display="none";
 }
 function send() {
-    var text = document.getElementById('text');
-    alert(text);
-    "
-    <div class="message">
-        <div class="message-text"><p>as</p></div>
-        <img class="message-image" src="style\img\me.jpg">
-        </div>"
+    var text = document.getElementById('text').value;
+
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            var message = document.createElement("div");
+            message.setAttribute("class","message");
+            var messagetext = document.createElement("div");
+            messagetext.setAttribute("class","message-text");
+            var p = document.createElement("p");
+            p.innerHTML=text;
+            var img = document.createElement("img");
+            img.setAttribute("class","message-image");
+            img.src="style/img/me.jpg";
+
+            messagetext.appendChild(p);
+            message.appendChild(messagetext);
+            message.appendChild(img);
+
+            document.getElementById('messages').appendChild(message);
+            document.getElementById('text').value="";
+        }
+    };
+    xhttp.open("POST", "http://51.15.59.130:46260/send", true);
+    obj = '{"message":"'+text+'"}';
+    xhttp.send(obj);
+
+
+}
+function getSupport() {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            var obj = JSON.parse(this.responseText);
+            document.getElementById('support-name').innerHTML=obj.support.first+" "+obj.support.last;
+            document.getElementById('support-image').src=obj.support.picture;
+            picture=obj.support.picture;
+        }
+    };
+    xhttp.open("GET", "http://51.15.59.130:46260/support", true);
+    xhttp.send();
+    setInterval(getResponse, 3000);
+}
+function getResponse() {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            var obj = JSON.parse(this.responseText);
+            var message = document.createElement("div");
+            message.setAttribute("class","response");
+            var messagetext = document.createElement("div");
+            messagetext.setAttribute("class","message-text");
+            var p = document.createElement("p");
+            p.innerHTML=obj.responses[0]['message'];
+            var img = document.createElement("img");
+            img.setAttribute("class","message-image");
+            img.src=picture;
+
+            messagetext.appendChild(p);
+            message.appendChild(img);
+            message.appendChild(messagetext);
+
+            document.getElementById('messages').appendChild(message);
+            document.getElementById('text').value="";
+        }
+    };
+    xhttp.open("GET", "http://51.15.59.130:46260/fetch", true);
+    xhttp.send();
 }
