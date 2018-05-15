78bb962df49784d38d768bca154d6e8abfe9eac6
Yousef <yousef.fatouraa@aut.ac.ir>
2018-05-15 22:07:10 +0430

    styles_complete_2

--- js/support.js
+++ js/support.js
@@ -9,3 +9,12 @@ function closeChat() {
     document.getElementById("support-closed").style.display="block";
     document.getElementById("support-open").style.display="none";
 }
+function send() {
+    var text = document.getElementById('text');
+    alert(text);
+    "
+    <div class="message">
+        <div class="message-text"><p>as</p></div>
+        <img class="message-image" src="style\img\me.jpg">
+        </div>"
+}
