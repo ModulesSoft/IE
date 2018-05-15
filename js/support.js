439d477ece6b758ef2785c06275bfc391cbb30eb
Yousef <yousef.fatouraa@aut.ac.ir>
2018-05-15 21:19:53 +0430

    styles_complete

--- js/support.js
+++ js/support.js
@@ -1,3 +1,11 @@
 /**
  * Created by Yousef on 18/05/15.
  */
+function openChat() {
+    document.getElementById("support-closed").style.display="none";
+    document.getElementById("support-open").style.display="block";
+}
+function closeChat() {
+    document.getElementById("support-closed").style.display="block";
+    document.getElementById("support-open").style.display="none";
+}
